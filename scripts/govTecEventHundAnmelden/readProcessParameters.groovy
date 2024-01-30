8c72f300a506df1bd0da40f8cde986f359890965
Dennis Weber <dennis.weber@seitenbau.com>
2024-01-30 09:20:12 +0100

    Add payment
    (not testet yet, will be done in another commit)

---
+++ scripts/govTecEventHundAnmelden/readProcessParameters.groovy
@@ -0,0 +1,26 @@
+package govTecEventHundAnmelden
+
+import de.seitenbau.serviceportal.scripting.api.v1.ScriptingApiV1
+
+final List<String> expectedParameters = [
+        "paymentUsername",
+        "paymentPassword",
+        "paymentAuthId",
+        "paymentAuthPassword",
+        "costInCent",
+]
+
+ScriptingApiV1 api = apiV1 // Variable is automatically set by Serviceportal process engine
+
+expectedParameters.each { parameterName ->
+  String parameterValue = api.getStartParameter().parameters.get(parameterName)
+  if (parameterValue == null || parameterValue.isEmpty()) {
+    throw new IllegalStateException("Failed to initialize process. Mandatory parameter '$parameterName' is not " +
+            "available. Please add this parameter to the Jesaja system.")
+  }
+}
+
+// Validate costInCent
+String costInCent = api.getStartParameter().parameters.get("costInCent")
+assert costInCent.matches(/\d+/): "Failed to initialize process. Parameter 'costInCent' was '${costInCent}', " +
+        "but most only contain digits."
--- scripts/govTecEventHundAnmelden/setupPayment.groovy
+++ scripts/govTecEventHundAnmelden/setupPayment.groovy
@@ -1,6 +1,7 @@
 package govTecEventHundAnmelden
 
 import de.seitenbau.serviceportal.scripting.api.v1.ScriptingApiV1
+import de.seitenbau.serviceportal.scripting.api.v1.payment.BerlinPaymentConfigV1
 import de.seitenbau.serviceportal.scripting.api.v1.payment.DisplayConfigV1
 import de.seitenbau.serviceportal.scripting.api.v1.payment.EpayBlPaymentConfigV1
 import de.seitenbau.serviceportal.scripting.api.v1.payment.PaymentConfigV1
@@ -13,21 +14,21 @@ ScriptingApiV1 api = apiV1 // Variable is automatically set by Serviceportal pro
 // Use `api.getStartParameter().parameters.get(parameterName)` to do so.
 
 // Setup paymentConfig
-PaymentConfigV1 paymentConfig = new EpayBlPaymentConfigV1() // TODO: Select correct type. Might be
-                                                            //  - EpayBlPaymentConfigV1, or
-                                                            //  - GiroCheckoutPaymentConfigV1, or
-                                                            //  - BerlinPaymentConfigV1, or
-                                                            //  - Epay21PaymentConfigV1, or
-                                                            //  - XBezahldienste1PaymentConfigV1
-                                                            //  See https://doku.pmp.seitenbau.com/display/DFO/paymentConfig
-paymentConfig.instanz = "bw" // TODO: Fill all required attributes, as described in their respective documentation.
+PaymentConfigV1 paymentConfig = new BerlinPaymentConfigV1()
+paymentConfig.username = api.getStartParameter().parameters.get("paymentUsername")
+paymentConfig.password = api.getStartParameter().parameters.get("paymentPassword")
+paymentConfig.authId = api.getStartParameter().parameters.get("paymentAuthId")
+paymentConfig.authPassword = api.getStartParameter().parameters.get("paymentAuthPassword\n")
 api.setVariable("paymentConfig", JsonOutput.prettyPrint(JsonOutput.toJson(paymentConfig)))
 
 // Setup transactionConfig
 // See https://doku.pmp.seitenbau.com/display/DFO/transactionConfig for expected values and other optional parameters
 TransactionConfigV1 transactionConfig = new TransactionConfigV1()
-transactionConfig.betrag = 1234 // TODO: Update
-transactionConfig.verwendungszweck = "TODO" // TODO: Update
+String costInCent = api.getStartParameter().parameters.get("costInCent")
+assert costInCent.isLong(): "Failed to configure payment. Process parameter 'costInCent' (value '$costInCent') " +
+        "cannot be cast to data type 'long'."
+transactionConfig.betrag = costInCent.toLong()
+transactionConfig.verwendungszweck = "GovTechEvent Demozahlung Hund"
 api.setVariable("transactionConfig", JsonOutput.prettyPrint(JsonOutput.toJson(transactionConfig)))
 
 // Setup displayConfig
